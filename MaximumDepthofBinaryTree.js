e53f6d507fac63db5319156c8a91cdd8233395c1
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-03-29 22:50:10 -0700

    Maximum Depth of Binary Tree Algorithm Method

---
+++ MaximumDepthofBinaryTree.js
@@ -0,0 +1,28 @@
+var maxDepth = function(root) {
+    if(!root) return []
+    
+    let result= [[root.val]]    
+    let queue = [[root]]
+    
+    while(queue.length>0){
+        let currentResult=[]
+        let current = queue.shift()
+        let tempArray=[]
+        while(current.length>0){
+            let temp = current.shift()
+            if(temp.left){
+                tempArray.push(temp.left)
+                currentResult.push(temp.left.val)
+            }
+            if(temp.right){
+                tempArray.push(temp.right)
+                currentResult.push(temp.right.val)
+            }  
+        }
+        tempArray.length >0 ? queue.push(tempArray) :  null
+        currentResult.length >0 ? result.push(currentResult) : null
+    }
+    
+   return result.length
+    
+};
