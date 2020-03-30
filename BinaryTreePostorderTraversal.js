61cc88692c9c61e62564139ca398ee52beebd2e6
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-03-29 22:46:36 -0700

    Add postorder Traversal Algorithm

---
+++ BinaryTreePostorderTraversal.js
@@ -0,0 +1,28 @@
+var postorderTraversal = function(root) {
+    
+    if(!root){
+        return []
+    }
+
+    let result =[]
+    let stackNodes=[]
+    let visted=[]
+    let current = root 
+    
+     stackNodes.push(current)
+    
+     while(stackNodes.length >0){
+        if(current.left && !visted.includes(current.left) ){
+            stackNodes.push(current)
+            current = current.left
+        }else if(current.right && !visted.includes(current.right) ){
+            stackNodes.push(current)
+            current = current.right
+        }else{
+            result.push(current.val)
+            visted.push(current)
+            current = stackNodes.pop()
+        }   
+    }
+        return result 
+};
