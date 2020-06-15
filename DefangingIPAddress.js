ba606766ce0d643f215f4c8097991f190094a19f
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-06-14 23:51:28 -0700

    Add Algorithm Defanging an IP Address

---
+++ DefangingIPAddress.js
@@ -0,0 +1,33 @@
+// Defanging an IP Address
+// Easy
+
+// 367
+
+// 780
+
+// Add to List
+
+// Share
+// Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+// A defanged IP address replaces every period "." with "[.]".
+
+ 
+
+// Example 1:
+
+// Input: address = "1.1.1.1"
+// Output: "1[.]1[.]1[.]1"
+// Example 2:
+
+// Input: address = "255.100.50.0"
+
+// /**
+//  * @param {string} address
+//  * @return {string}
+//  */
+var defangIPaddr = function(address) {
+    return address.split(".").join("[.]")
+    
+    
+};
